d55e3da2045779014ca6df23295a3b660163dcf0
Addimator <adpri100@hhu.de>
2023-09-08 08:26:29 +0200

    Refactor creating scatter plot

--- workflow/scripts/scatter_plot.py
+++ workflow/scripts/scatter_plot.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot as plt
 
-# Pfad zur Bedgraph-Datei
-bedgraph_path = snakemake.input["bedGraph"]
-vcf_path = snakemake.input["calls"]
 
-# Initialize dictionaries to store position-value mappings
 bedgraph_dict = {}
 vcf_dict = {}
 
-# 2. Create dictionaries for Bedgraph and VCF
-with open(vcf_path, 'r') as vcf_file, open(bedgraph_path, 'r') as bedgraph_file:
+# Create dictionaries for Bedgraph and VCF
+with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["bedGraph"], 'r') as bedgraph_file:
 
     for line in bedgraph_file:
         if not line.startswith("track"):
             parts = line.strip().split('\t')
-            chrom, position, methylation_value = parts[0], int(parts[1]) + int(parts[2]) // 2, int(parts[3])
-            # Store the position and methylation value in the dictionary
+            chrom, position, methylation_value = parts[0], (int(parts[1]) + int(parts[2])) // 2, int(parts[3])
             bedgraph_dict[(chrom, position)] = methylation_value
 
     for line in vcf_file:
@@ -26,46 +21,39 @@ with open(vcf_path, 'r') as vcf_file, open(bedgraph_path, 'r') as bedgraph_file:
             af_index = info_field.split(":").index("AF")
             format_values = parts[9].split(":")
             af_value = float(format_values[af_index])
-            # Füge den AF-Wert zur Liste hinzu
             vcf_dict[(chrom, pos)] = af_value
  
 
-""" # Print the dictionaries
-print("Bedgraph Dictionary:")
-for key, value in bedgraph_dict.items():
-    print(f'Position: {key}, Methylation Value: {value}') """
 
-# print("VCF Dictionary:")
-# for key, value in vcf_dict.items():
-#     print(f'Position: {key}, AF Value: {value}')
+bedgraph_positions = [key for key in bedgraph_dict if key in vcf_dict]
+bedgraph_methylation_values = [bedgraph_dict[key] for key in bedgraph_positions]
 
+vcf_positions = [key for key in vcf_dict if key in bedgraph_dict]
+vcf_af_values = [vcf_dict[key] * 100 for key in vcf_positions]
 
+missing_positions1 = [key for key in bedgraph_dict if key not in vcf_dict]
+missing_positions2 = [key for key in vcf_dict if key not in bedgraph_dict]
 
-# 3. Scatterplot and remove redundant positions
-bedgraph_positions = []
-bedgraph_methylation_values = []
-vcf_positions = []
-vcf_af_values = []
-test = True
-
-
-for key, value in bedgraph_dict.items():
-    if key in list(vcf_dict.keys()):
-        bedgraph_positions.append(key)
-        bedgraph_methylation_values.append(value)
-
-for key, value in vcf_dict.items():
-    if key in list(bedgraph_dict.keys()):
-
-        vcf_positions.append(key)
-        vcf_af_values.append(value * 100)
 
 
 # print(bedgraph_methylation_values)
 # Create a scatterplot
-plt.scatter(vcf_af_values, bedgraph_methylation_values, alpha=0.5)
-plt.xlabel('AF Values (Percentage)')
+fig, ax = plt.subplots()
+ax.annotate(
+    f"Missing positions in Varlociraptor:{missing_positions1}. \nMissing positions in MethylDackel:{missing_positions2}.",
+    xy=(0.5, -0.1),  # Koordinaten der Textposition (x, y)
+    xycoords='axes fraction',  # Verwenden Sie die Koordinatenachse des Plots
+    fontsize=6,
+    ha="center",
+    va="center"
+)
+plt.plot([1, 100], [1, 100], label='Proportionale Ausgleichsgerade', color='red')
+
+plt.scatter(vcf_af_values, bedgraph_methylation_values, alpha=1)
+plt.xlabel('AF Values')
 plt.ylabel('Methylation Values')
-plt.title('Scatterplot: Methylation vs. AF')
+plt.title('MethylDackel vs. Varlociraptor')
+
+
+
 plt.savefig(snakemake.output[0])
-plt.show()
