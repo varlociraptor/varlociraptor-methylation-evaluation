5baf9c1a167b6cefffd282b21dfeef2f50d8fd41
Addimator <adpri100@hhu.de>
2023-09-11 16:28:37 +0200

    Snakefile for paired end and single end

--- workflow/scripts/scatter_plot.py
+++ workflow/scripts/scatter_plot.py
@@ -15,6 +15,7 @@ with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["bedG
             chrom, position, methylation_value = parts[0], (int(parts[1]) + int(parts[2])) // 2, int(parts[3])
             bedgraph_dict[(chrom, position)] = methylation_value
 
+
     for line in vcf_file:
         if not line.startswith('#'):
             parts = line.strip().split('\t')
@@ -26,29 +27,25 @@ with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["bedG
             vcf_dict[(chrom, pos)] = af_value
  
     true_meth_data = ch3_file.readlines()
-    # Durchlaufen Sie die Zeilen der CH3-Datei
     for i in range(len(true_meth_data) - 1):
-        # Teilen Sie die Zeilen an Tabulatorzeichen auf
         parts1 = true_meth_data[i].strip().split('\t')
         parts2 = true_meth_data[i + 1].strip().split('\t')
         
-        # Überprüfen Sie, ob es sich um benachbarte CG-Methylierungen handelt
-        if parts1[3] == 'CG' and parts2[3] == 'CG':
+        if parts1[3] == 'CG' and parts2[3] == 'CG' and parts1[2] == "+":
             chrom = parts1[0]
-            position1 = int(parts1[1])  # Die Position der ersten CG-Methylierung
-            position2 = int(parts2[1])  # Die Position der zweiten CG-Methylierung
-            methylation1 = float(parts1[6])  # Der Methylierungsgrad der ersten CG-Methylierung
-            methylation2 = float(parts2[6])  # Der Methylierungsgrad der zweiten CG-Methylierung
+            position1 = int(parts1[1])  
+            position2 = int(parts2[1]) 
+            methylation1 = float(parts1[6]) 
+            methylation2 = float(parts2[6])  
 
-            # Berechnen Sie den Durchschnitt der Methylierungsgrade
             avg_methylation = (methylation1 + methylation2) / 2
-
-            # Fügen Sie die Position und den Methylierungsgrad hinzu
-            ch3_dict[(chrom, position1)] = avg_methylation
+            ch3_dict[(chrom, position2)] = avg_methylation
             i += 1
 
+print(len(bedgraph_dict), len(vcf_dict), len(ch3_dict))
 
 bedgraph_positions = [key for key in bedgraph_dict if key in vcf_dict and key in ch3_dict]
+print(len(bedgraph_positions))
 bedgraph_meth_values = [bedgraph_dict[key] for key in bedgraph_positions]
 
 vcf_positions = [key for key in vcf_dict if key in bedgraph_dict and key in ch3_dict]
@@ -57,9 +54,11 @@ vcf_af_values = [vcf_dict[key] * 100 for key in vcf_positions]
 ch3_positions = [key for key in ch3_dict if key in bedgraph_dict and key in vcf_dict]
 ch3_meth_values = [ch3_dict[key] * 100 for key in ch3_positions]
 
-missing_positions1 = [key for key in bedgraph_dict if key not in vcf_dict]
-missing_positions2 = [key for key in vcf_dict if key not in bedgraph_dict]
+missing_positions1 = [key for key in bedgraph_dict if key not in vcf_dict and key not in ch3_dict]
+missing_positions2 = [key for key in vcf_dict if key not in bedgraph_dict and key not in ch3_dict]
+missing_positions3 = [key for key in ch3_dict if key not in bedgraph_dict and key not in vcf_dict]
 
+print(missing_positions3)
 
 
 
@@ -72,6 +71,7 @@ line = alt.Chart(pd.DataFrame({'x': [1, 100], 'y': [1, 100]})).mark_line(color='
 )
 
 # Plot TrueMeth vs Varlociraptor
+deviation = sum(abs(x - y) for x, y in zip(ch3_meth_values, vcf_af_values))
 
 data = pd.DataFrame({
     'TrueMeth': ch3_meth_values,
@@ -86,12 +86,16 @@ scatter = alt.Chart(data).mark_circle(opacity=0.5).encode(
 final_chart = (scatter + line).properties(
     width=400,
     height=400, 
-    title='TrueMeth vs. Varlociraptor'
+    title=f'TrueMeth vs. Varlociraptor (Deviation: {deviation})'
 )
 final_chart.save(snakemake.output["tv"], scale_factor=2.0) 
 
 
+
 # Plot MethylDackel vs Varlociraptor
+
+deviation = sum(abs(x - y) for x, y in zip(bedgraph_meth_values, vcf_af_values))
+
 
 data = pd.DataFrame({
     'MethylDackel': bedgraph_meth_values,
@@ -106,12 +110,16 @@ scatter = alt.Chart(data).mark_circle(opacity=0.5).encode(
 final_chart = (scatter + line).properties(
     width=400,
     height=400, 
-    title='MethylDackel vs. Varlociraptor'
+    title=f'MethylDackel vs. Varlociraptor (Deviation: {deviation})'
 )
 final_chart.save(snakemake.output["dv"], scale_factor=2.0) 
 
 
 # Plot TrueMeth vs MethylDackel
+deviation = sum(abs(x - y) for x, y in zip(ch3_meth_values, bedgraph_meth_values))
+
+print(len(bedgraph_meth_values))
+print(len(bedgraph_dict))
 
 data = pd.DataFrame({
     'TrueMeth': ch3_meth_values,
@@ -126,7 +134,7 @@ scatter = alt.Chart(data).mark_circle(opacity=0.5).encode(
 final_chart = (scatter + line).properties(
     width=400,
     height=400, 
-    title='TrueMeth vs. MethylDackel'
+    title=f'TrueMeth vs. MethylDackel (Deviation: {deviation})'
 )
 final_chart.save(snakemake.output["td"], scale_factor=2.0) 
 
