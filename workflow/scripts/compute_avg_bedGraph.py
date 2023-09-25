7663d987f22895764d21b84a5c1d6f6376bb2a96
Addimator <adpri100@hhu.de>
2023-09-25 12:33:06 +0200

    Focus on chromosome

--- workflow/scripts/compute_avg_bedGraph.py
+++ workflow/scripts/compute_avg_bedGraph.py
@@ -17,7 +17,7 @@ for file_path in bedGraph_files:
         for line in file:
             parts = line.strip().split("\t")
             chrom, start, end, methylation, meth_reads, unmeth_reads = parts
-            if chrom == snakemake.params[chromosome]:
+            if chrom == snakemake.params["chromosome"]:
                 methylation = float(methylation)
                 meth_reads = int(meth_reads)
                 unmeth_reads = int(unmeth_reads)
