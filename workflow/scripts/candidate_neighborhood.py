6bd4b57014becf70f4d899272021733df1cf7858
Addimator <adpri100@hhu.de>
2023-11-08 11:46:13 +0100

    Change reads invalid and filter mutations

---
+++ workflow/scripts/candidate_neighborhood.py
@@ -0,0 +1,14 @@
+candidate = snakemake.params["candidate"]
+genome = snakemake.input["genome"]
+
+# Die x-te Position aus der FASTA-Datei auslesen
+with open(genome, "r") as fasta_file:
+    header = fasta_file.readline()  # Die erste Zeile (Header) lesen und überspringen
+    sequence = "".join([line.strip() for line in fasta_file])  # Die restlichen Zeilen einlesen
+    position_x = sequence[candidate - 1]
+
+print(sequence[0:50])
+left = candidate - 150 if candidate - 150 > 0 else 0
+right= candidate + 150 if candidate + 150 < len(sequence) else len(sequence)
+
+print(sequence[left:candidate-1], " ", sequence[candidate-1], " ", sequence[candidate:right])
