784406f8c459206a8b63bbe9eeb925e7b05fc041
Addimator <adpri100hhu.de>
2024-01-15 11:29:34 +0000

    Add pacbio and nanopore methylation caller comparision

--- workflow/scripts/scatter_plot_nanopore.py
+++ workflow/scripts/scatter_plot_nanopore.py
@@ -4,20 +4,14 @@ import numpy as np
 
 import heapq
 
+def euclidian_distance(x1, y1, x2, y2):
+    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-dackel_dict = {}
 vcf_dict = {}
 true_dict = {}
 
 # Create dictionaries for Bedgraph and VCF
-with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["bedGraph"], 'r') as dackel_file, open(snakemake.input["true_meth"][0], 'r') as truth_file:
-
-    for line in dackel_file:
-        if not line.startswith("track"):
-            parts = line.strip().split('\t')
-            chrom, position, methylation_value = parts[0], (int(parts[1]) + int(parts[2])) // 2, float(parts[3])
-            dackel_dict[(chrom, position)] = methylation_value
-
+with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["true_meth"], 'r') as truth_file:
 
     for line in vcf_file:
         if not line.startswith('#'):
@@ -40,21 +34,18 @@ with open(snakemake.input["calls"], 'r') as vcf_file, open(snakemake.input["bedG
 # vcf_dict = {key: value for key, value in vcf_dict.items() if value > 0.3}
 
 
-bedgraph_positions = [key for key in dackel_dict if key in vcf_dict and key in true_dict]
-bedgraph_meth_values = [dackel_dict[key] for key in bedgraph_positions]
 
-vcf_positions = [key for key in vcf_dict if key in dackel_dict and key in true_dict]
+vcf_positions = [key for key in vcf_dict if key in true_dict]
 vcf_af_values = [vcf_dict[key] * 100 for key in vcf_positions]
 
 
 true_dict = dict(sorted(true_dict.items(), key=lambda item: item[0][1]))
-true_positions = [key for key in true_dict if key in dackel_dict and key in vcf_dict]
+true_positions = [key for key in true_dict if  key in vcf_dict]
 true_meth_values = [true_dict[key] for key in true_positions]
 
 
-missing_positions1 = [key for key in dackel_dict if key not in vcf_dict and key not in true_dict]
-missing_positions2 = [key for key in vcf_dict if key not in dackel_dict and key not in true_dict]
-missing_positions3 = [key for key in true_dict if key not in dackel_dict and key not in vcf_dict]
+missing_positions2 = [key for key in vcf_dict if key not in true_dict]
+missing_positions3 = [key for key in true_dict if key not in vcf_dict]
 
 
 # with open(snakemake.output["test"], "w") as datei:
@@ -62,6 +53,8 @@ missing_positions3 = [key for key in true_dict if key not in dackel_dict and key
 #         if abs(el - vcf_af_values[i]) > 20: 
 #             datei.write(str(true_positions[i]) + "\t" + str(el) + "\n")
 #             datei.write("Bedgraph_value:" + str(bedgraph_meth_values[i]) + "\t Callsvcf:  " + str(vcf_af_values[i]) + "\n\n")
+
+
 def euclidian_distance(x1, y1, x2, y2):
     return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 ############################################################################################################################################################
@@ -89,9 +82,9 @@ sorted_list = sorted(distances, reverse=True)
 deviation = sum(distances) / len(true_meth_values)
 
 
-# Debug:
+# # Debug:
 # for i, (x, y) in enumerate(zip(true_meth_values, vcf_af_values)):
-#     if euclidian_distance(x, y, x, x) > 40 and x > 90:
+#     if euclidian_distance(x, y, x, x) < 1 and x < 90:
 #         print(euclidian_distance(x, y, x, x), x, y)
 #         print(vcf_positions[i])
 
@@ -106,6 +99,11 @@ chart = alt.Chart(data).mark_bar().encode(
 )
 chart.save(snakemake.output["dist_tv"], scale_factor=2.0) 
 
+for i, (x, y) in enumerate(zip(true_meth_values, vcf_af_values)):
+    if abs(x - y) > 80:
+        print(x, y, true_positions[i])
+
+# print(true_positions)
 
 data = pd.DataFrame({
     'TrueMeth': true_meth_values,
@@ -123,79 +121,4 @@ final_chart = (scatter + line).properties(
     title=f'TrueMeth vs. Varlociraptor (Deviation: {deviation})'
 )
 final_chart.save(snakemake.output["tv"], scale_factor=2.0) 
-
-
-
-# Plot MethylDackel vs Varlociraptor
-distances = [euclidian_distance(x, y, x, x) for x, y in zip(bedgraph_meth_values, vcf_af_values)]
-sorted_list = sorted(distances, reverse=True)
-deviation = sum(distances) / len(bedgraph_meth_values)
-
-
-for i, (x, y) in enumerate(zip(bedgraph_meth_values, vcf_af_values)):
-    if euclidian_distance(x, y, x, x) > 60 and x > 90:
-        print(euclidian_distance(x, y, x, x), x, y)
-        print(vcf_positions[i])
-
-
-data = pd.DataFrame({
-    'MethylDackel': bedgraph_meth_values,
-    'Varlociraptor': vcf_af_values
-})
-
-scatter = alt.Chart(data).mark_circle(opacity=0.5).encode(
-    x='MethylDackel',
-    y='Varlociraptor'
-)
-
-final_chart = (scatter + line).properties(
-    width=400,
-    height=400, 
-    title=f'MethylDackel vs. Varlociraptor (Deviation: {deviation})'
-)
-final_chart.save(snakemake.output["dv"], scale_factor=2.0) 
-
-
-# Plot TrueMeth vs MethylDackel
-distances = [euclidian_distance(x, y, x, x) for x, y in zip(bedgraph_meth_values, true_meth_values)]
-sorted_list = sorted(distances, reverse=True)
-deviation = sum(distances) / len(bedgraph_meth_values)
-# Runde die Kommazahlen auf ganze Zahlen
-rounded_distances = [int(round(d)) for d in distances]
-
-# Erzeuge ein Pandas DataFrame
-data = pd.DataFrame({'Rounded_Distances': rounded_distances})
-
-# Erzeuge das Altair-Plot
-chart = alt.Chart(data).mark_bar().encode(
-    x=alt.X('Rounded_Distances:O', axis=alt.Axis(title='distance')),
-    y=alt.Y('count():Q', axis=alt.Axis(title='number'))
-).properties(
-    title='Distance distribution MethylDackel'
-)
-
-chart.save(snakemake.output["dist_td"], scale_factor=2.0) 
-
-
-
-data = pd.DataFrame({
-    'TrueMeth': true_meth_values,
-    'MethylDackel': bedgraph_meth_values
-})
-
-scatter = alt.Chart(data).mark_circle(opacity=0.5).encode(
-    x='TrueMeth',
-    y='MethylDackel'
-)
-
-final_chart = (scatter + line).properties(
-    width=400,
-    height=400, 
-    title=f'TrueMeth vs. MethylDackel (Deviation: {deviation})'
-)
-final_chart.save(snakemake.output["td"], scale_factor=2.0) 
-
-
-
-
 
