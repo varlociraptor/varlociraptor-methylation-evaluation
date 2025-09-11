a16946a7a67db0c95e0eea390374c4f74833026c
Addimator <adpri100@hhu.de>
2025-09-12 00:20:47 +0200

    Heatmaps now working for multiple reference tools

--- workflow/scripts/heatmap_replicates.py
+++ workflow/scripts/heatmap_replicates.py
@@ -16,14 +16,66 @@ def bin_methylation(series, bin_size=10):
     return (np.round(series / bin_size) * bin_size).astype(int)
 
 
+def plot_diff_heatmap(method, df_dict):
+    df1 = df_dict[method]
+    df2 = df_dict["varlo"]
+    df_diff = df1.merge(
+        df2, on=["rep1_bin", "rep2_bin"], suffixes=(f"_varlo", f"_{method}")
+    )
+    df_diff["abs_diff"] = (df_diff[f"count_varlo"] - df_diff[f"count_{method}"]).abs()
+    df_diff["log2FC"] = np.where(
+        (df_diff[f"count_varlo"] != 0) & (df_diff[f"count_{method}"] != 0),
+        np.log2(df_diff[f"count_varlo"] / df_diff[f"count_{method}"]).abs(),
+        0,
+    )
+
+    # Difference Heatmap
+    diff_heatmap = (
+        alt.Chart(df_diff)
+        .mark_rect()
+        .encode(
+            x=alt.X("rep1_bin:O", sort=list(range(0, 101, 10))),
+            y=alt.Y("rep2_bin:O", sort=list(range(100, -1, -10))),
+            color=alt.Color(
+                "abs_diff:Q",
+                scale=alt.Scale(
+                    type="log", scheme="viridis", domain=[1, df_diff["abs_diff"].max()]
+                ),
+            ),
+            tooltip=["rep1_bin", "rep2_bin", "abs_diff"],
+        )
+        .properties(title=f"Abs Difference {method} vs varlo", width=150, height=150)
+    )
+
+    # Ratio Heatmap
+    ratio_heatmap = (
+        alt.Chart(df_diff)
+        .mark_rect()
+        .encode(
+            x=alt.X("rep1_bin:O", sort=list(range(0, 101, 10))),
+            y=alt.Y("rep2_bin:O", sort=list(range(100, -1, -10))),
+            color=alt.Color(
+                "log2FC:Q",
+                scale=alt.Scale(scheme="viridis", domain=[0, df_diff["log2FC"].max()]),
+            ),
+            tooltip=["rep1_bin", "rep2_bin", "log2FC"],
+        )
+        .properties(title=f"Log2 Fold Change {method}/varlo", width=150, height=150)
+    )
+
+    return alt.vconcat(diff_heatmap, ratio_heatmap).resolve_scale(color="independent")
+
+
 def plot_heatmap_meth_callers(df_dict):
     sample_name = snakemake.params["protocol"]
     df = df_dict[sample_name]
 
     meth_caller_dfs = {}
+    method_plots = []
 
     # Counts für jede meth_callere berechnen
     for meth_caller in snakemake.params["meth_callers"]:
+        print(f"Methode: {meth_caller}", file=sys.stderr)
         x_col = f"{meth_caller}_methylation_rep1"
         y_col = f"{meth_caller}_methylation_rep2"
 
@@ -40,20 +92,6 @@ def plot_heatmap_meth_callers(df_dict):
         )
         meth_caller_dfs[meth_caller] = agg
 
-    # Wir gehen davon aus: genau 2 meth_calleren
-    m1, m2 = snakemake.params["meth_callers"]
-    df_diff = meth_caller_dfs[m1].merge(
-        meth_caller_dfs[m2], on=["rep1_bin", "rep2_bin"], suffixes=(f"_{m1}", f"_{m2}")
-    )
-    df_diff["abs_diff"] = (df_diff[f"count_{m1}"] - df_diff[f"count_{m2}"]).abs()
-    df_diff["log2FC"] = np.where(
-        (df_diff[f"count_{m1}"] != 0) & (df_diff[f"count_{m2}"] != 0),
-        np.log2(df_diff[f"count_{m1}"] / df_diff[f"count_{m2}"]).abs(),
-        0,
-    )
-    # Original Heatmaps
-    charts = []
-    for meth_caller, agg in meth_caller_dfs.items():
         heatmap = (
             alt.Chart(agg)
             .mark_rect()
@@ -78,47 +116,14 @@ def plot_heatmap_meth_callers(df_dict):
             )
             .properties(title=f"{meth_caller} Heatmap", width=150, height=150)
         )
-        charts.append(heatmap)
 
-    # Difference Heatmap
-    diff_heatmap = (
-        alt.Chart(df_diff)
-        .mark_rect()
-        .encode(
-            x=alt.X("rep1_bin:O", sort=list(range(0, 101, 10))),
-            y=alt.Y("rep2_bin:O", sort=list(range(100, -1, -10))),
-            color=alt.Color(
-                "abs_diff:Q",
-                scale=alt.Scale(
-                    type="log", scheme="viridis", domain=[1, df_diff["abs_diff"].max()]
-                ),
-            ),
-            tooltip=["rep1_bin", "rep2_bin", "abs_diff"],
-        )
-        .properties(title=f"Abs Difference {m1} vs {m2}", width=150, height=150)
-    )
+        method_plots.append(heatmap)
+        print("Methode fertig", method_plots, file=sys.stderr)
 
-    # Ratio Heatmap
-    ratio_heatmap = (
-        alt.Chart(df_diff)
-        .mark_rect()
-        .encode(
-            x=alt.X("rep1_bin:O", sort=list(range(0, 101, 10))),
-            y=alt.Y("rep2_bin:O", sort=list(range(100, -1, -10))),
-            color=alt.Color(
-                "log2FC:Q",
-                scale=alt.Scale(scheme="viridis", domain=[0, df_diff["log2FC"].max()]),
-            ),
-            tooltip=["rep1_bin", "rep2_bin", "log2FC"],
-        )
-        .properties(title=f"Log2 Fold Change {m1}/{m2}", width=150, height=150)
+    return (
+        alt.hconcat(*method_plots).resolve_scale(color="independent"),
+        meth_caller_dfs,
     )
-
-    tool_charts = alt.hconcat(*charts).resolve_scale(color="independent")
-    diff_charts = alt.hconcat(diff_heatmap, ratio_heatmap).resolve_scale(
-        color="independent"
-    )
-    return alt.vconcat(tool_charts, diff_charts).resolve_scale(color="independent")
 
 
 # Daten aus HDF5 einlesen
@@ -128,8 +133,17 @@ with pd.HDFStore(snakemake.input[0], mode="r", locking=False) as store:
         meth_caller_dfs[key.strip("/")] = store[key]
 
 # Heatmaps erstellen
-chart = plot_heatmap_meth_callers(meth_caller_dfs)
+chart, meth_caller_dfs = plot_heatmap_meth_callers(meth_caller_dfs)
+diff_charts = []
+# Differenz-Heatmaps erstellen
+for method in snakemake.params["meth_callers"]:
+    if method == "varlo":
+        continue  # Differenz-Heatmap von varlo zu varlo macht keinen Sinn
+    diff_chart = plot_diff_heatmap(method, meth_caller_dfs)
+    diff_charts.append(diff_chart)
 
+diff_chart_plots = alt.hconcat(*diff_charts).resolve_scale(color="independent")
+chart = alt.vconcat(chart, diff_chart_plots)
 # Chart speichern
 chart.save(
     snakemake.output[0],
