3b63207da3aef7becf5524ba78366a8ecd96c5a7
Addimator <adrian.prinz@stud.uni-due.de>
2026-06-23 06:20:00 +0000

    Transform precision recall plot to positive negative rate plot

--- workflow/scripts/compute_precision_recall.py
+++ workflow/scripts/compute_precision_recall.py
@@ -40,25 +40,53 @@ def compute_precision_recall(df, meth_callers):
     results = []
 
     for caller in meth_callers:
-        df = df.with_columns(
+        df_caller = df.filter(pl.col(f"{caller}_methylation").is_not_null())
+        df_caller = df_caller.with_columns(
             pl.when(pl.col(f"{caller}_methylation") > 0)
             .then(1)
             .otherwise(0)
             .alias(f"{caller}_binary")
         )
 
-        TP = ((df[f"{caller}_binary"] == 1) & (df["truth_binary"] == 1)).sum()
-        FP = ((df[f"{caller}_binary"] == 1) & (df["truth_binary"] == 0)).sum()
-        FN = ((df[f"{caller}_binary"] == 0) & (df["truth_binary"] == 1)).sum()
-
+        TP = (
+            (df_caller[f"{caller}_binary"] == 1) & (df_caller["truth_binary"] == 1)
+        ).sum()
+        FP = (
+            (df_caller[f"{caller}_binary"] == 1) & (df_caller["truth_binary"] == 0)
+        ).sum()
+        FN = (
+            (df_caller[f"{caller}_binary"] == 0) & (df_caller["truth_binary"] == 1)
+        ).sum()
+        TN = (
+            (df_caller[f"{caller}_binary"] == 0) & (df_caller["truth_binary"] == 0)
+        ).sum()
+        P = df_caller[f"{caller}_binary"].sum()
+        N = df_caller.height - P
         precision = TP / (TP + FP) if (TP + FP) > 0 else 0.0
         recall = TP / (TP + FN) if (TP + FN) > 0 else 0.0
+        positive_rate = TP / P if P > 0 else 0.0
+        negative_rate = TN / N if N > 0 else 0.0
 
+        print(
+            f"caller: {caller}:",
+            "\n\tTP: ",
+            TP,
+            "\n\tTN: ",
+            TN,
+            "\n\tP: ",
+            P,
+            "\n\tN: ",
+            N,
+            "\n\tpositive_rate: ",
+            positive_rate,
+            "\n\tnegative_rate: ",
+            negative_rate,
+        )
         results.append(
             {
                 "caller": caller,
-                "precision": precision,
-                "recall": recall,
+                "positive_rate": positive_rate,
+                "negative_rate": negative_rate,
             }
         )
 
@@ -67,9 +95,10 @@ def compute_precision_recall(df, meth_callers):
 
 alt.data_transformers.enable("vegafusion")
 meth_callers = snakemake.params.meth_callers
-truth_df = pl.read_csv(snakemake.input[0])
-tools_df = pl.read_parquet(snakemake.input[1])
-
+truth_df = pl.read_csv(snakemake.input[0], schema_overrides={"chrom": pl.Utf8})
+tools_df = pl.read_parquet(snakemake.input[1]).with_columns(
+    pl.col("chromosome").cast(pl.Utf8)
+)
 df = truth_df.join(
     tools_df,
     left_on=["chrom", "pos"],
@@ -89,19 +118,16 @@ metrics_df = compute_precision_recall(df, meth_callers)
 metrics_df = metrics_df.with_columns(
     pl.col("caller").replace(meth_caller_to_name).alias("tool_name")
 )
-print(metrics_df)
 color_domain = (
     metrics_df.select(pl.col("tool_name").unique()).to_series().sort().to_list()
 )
-print(color_domain)
 color_range = [tool_colors[t] for t in color_domain]
-print(color_range)
 chart = (
     alt.Chart(metrics_df.to_pandas())
     .mark_point(size=150)
     .encode(
-        x=alt.X("recall:Q", title="Recall"),
-        y=alt.Y("precision:Q", title="Precision"),
+        x=alt.X("positive_rate:Q", title="Positive Rate"),
+        y=alt.Y("negative_rate:Q", title="Negative Rate"),
         color=alt.Color(
             "tool_name:N",
             scale=alt.Scale(
@@ -110,9 +136,9 @@ chart = (
             ),
             title="Methylation caller",
         ),
-        tooltip=["caller", "precision", "recall"],
+        tooltip=["caller", "positive_rate", "negative_rate"],
     )
-    .properties(width=500, height=400, title="Precision vs Recall")
+    .properties(width=500, height=400, title="Positive Rate vs Negative Rate")
 )
 
 chart.save(snakemake.output[0])
